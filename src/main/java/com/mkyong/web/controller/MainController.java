714eb01eab550e1aad767408f3383162f07ef0b2
OlegPalchyk <olegpa@interlink-ua.com>
2015-11-11 17:14:24 +0200

    Spring security with MySQL

--- src/main/java/com/mkyong/web/controller/MainController.java
+++ src/main/java/com/mkyong/web/controller/MainController.java
@@ -1,25 +1,24 @@
-/*
 package com.mkyong.web.controller;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-
 @Controller
-public class HelloController {
+public class MainController {
 
     @RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
-    public ModelAndView welcomePage() {
+    public ModelAndView defaultPage() {
 
         ModelAndView model = new ModelAndView();
-        model.addObject("title", "Spring Security Custom Login Form");
-        model.addObject("message", "This is welcome page!");
+        model.addObject("title", "Spring Security Login Form - Database Authentication");
+        model.addObject("message", "This is default page!");
         model.setViewName("hello");
         return model;
 
@@ -29,19 +28,16 @@ public class HelloController {
     public ModelAndView adminPage() {
 
         ModelAndView model = new ModelAndView();
-        model.addObject("title", "Spring Security Custom Login Form");
-        model.addObject("message", "This is protected page!");
+        model.addObject("title", "Spring Security Login Form - Database Authentication");
+        model.addObject("message", "This page is for ROLE_ADMIN only!");
         model.setViewName("admin");
-
         return model;
 
     }
 
-    //Spring Security see this :
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public ModelAndView login(
-            @RequestParam(value = "error", required = false) String error,
-            @RequestParam(value = "logout", required = false) String logout) {
+    public ModelAndView login(@RequestParam(value = "error", required = false) String error,
+                              @RequestParam(value = "logout", required = false) String logout) {
 
         ModelAndView model = new ModelAndView();
         if (error != null) {
@@ -57,10 +53,23 @@ public class HelloController {
 
     }
 
+    //for 403 access denied page
+    @RequestMapping(value = "/403", method = RequestMethod.GET)
+    public ModelAndView accesssDenied() {
 
+        ModelAndView model = new ModelAndView();
 
+        //check if user is login
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (!(auth instanceof AnonymousAuthenticationToken)) {
+            UserDetails userDetail = (UserDetails) auth.getPrincipal();
+            model.addObject("username", userDetail.getUsername());
+        }
 
+        model.setViewName("403");
+        return model;
 
+    }
 
 }
-*/
+
